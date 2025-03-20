ce706d80c43f7d6adc109f5bc806b90081be5012
Michciook <oskarmichta@gmail.com>
2025-03-20 22:35:09 +0100

    added get_rooms route

--- api-server-flask/website/views.py
+++ api-server-flask/website/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
-
+from flask_jwt_extended import create_access_token
+from . import db
+from .models import Room
 
 views = Blueprint('views', __name__)
 
@@ -13,3 +15,14 @@ def test():
         return jsonify({'test': 'this is a test data'}), 200
     except Exception as e:
         return jsonify({'error': f'Internal server error: {str(e)}'}), 500
+    
+@views.route('/api/get_rooms/', methods=['GET'])
+def get_rooms():
+    try:
+        room_rows = db.session.query(Room).all()
+        data = [{'id': row.room_id, 'roomname': row.roomname, 'creator_id': row.creator_id } for row in room_rows]
+        return jsonify(data), 200
+    except Exception as e:
+        return jsonify({'error': f'Internal server error: {str(e)}'}), 500
+    
+
